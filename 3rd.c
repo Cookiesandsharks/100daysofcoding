8f66383fd04812de7d43264cf054b182d5f73af7
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 16:06:08 +0530

    3rd.c

--- 3rd.c
+++ 3rd.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 int main() {
-    int num1;
-    int num2;
-    int sum;
-    int diff;
-    int pro;
-    int quo;
-    printf("Enter 1st number:");
-    scanf("%d",&num1);
-    printf("Enter 2nd number:");
-    scanf("%d",&num2);
-    sum=num1+num2;
-    printf("Sum of the given numbers:%d\n",sum);
-    diff=num1-num2;
-    printf("Difference of the guven numbers are:%d\n",diff);
-    pro=num1*num2;
-    printf("Product of the given mumbers are:%d\n",pro);
-    quo=num1/num2;
-    printf("Qoutient of the given numbers are:%d\n",quo);
+    int area;
+    int perimeter;
+    int lenght;
+    int breadth;
+    printf("Enter lenght of rectangle:");
+    scanf("%d",&lenght);
+    printf("Enter breadth of rectangle:");
+    scanf("%d",&breadth);
+    perimeter=2*lenght+2*breadth;
+    printf("perimeter of the rectangle is:%d\n",perimeter);
+    area=lenght*breadth;
+    printf("area of the rectangle:%d\n", area);
     return 0;
 }
