689a01faa00315b294f0a6ac4f095da04832de83
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 15:53:45 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num1;
+    int num2;
+    int sum;
+    printf("Enter 1st number:");
+    scanf("%d",&num1);
+    printf("Enter 2nd number:");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("Sum of the given numbers:%d",sum);
+    return 0;
+}
