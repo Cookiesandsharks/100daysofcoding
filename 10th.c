df1b19c9cc1577c6fc71b15b00bd701491c173e7
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 23:12:58 +0530

    Add files via upload

---
+++ 10th.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int sec;
+    int min;
+    int h;
+    int x;
+    printf("Enter seconds to be converted:");
+    scanf("%d",&sec);
+    h=sec/3600;
+    x=sec%3600;
+    min=x/60;
+    sec=x%60;
+    printf("%d:%d:%d",h,min,sec);
+    return 0;
+}
