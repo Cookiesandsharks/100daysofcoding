c1d1d001f1a43c74d81fad5bf6ce41926e3b3f83
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 16:33:49 +0530

    Add files via upload

---
+++ 6th.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int x;
+    int y;
+    int a;
+    printf("Enter 1st number:");
+    scanf("%d",&x);
+    printf("Enter 2nd number:");
+    scanf("%d",&y);
+    printf("Before swap first number is:%d\n",x);
+    printf("Before swap second number is:%d\n",y);
+    a=x;
+    x=y;
+    y=a;
+    printf("After swap 1st:%d\n",x);
+    printf("After swap 2nd:%d\n",y);
+    return 0;
+};
