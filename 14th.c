59433830d070399c5e03135dc91950c6ec819054
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-28 00:46:34 +0530

    14th.c

--- 14th.c
+++ 14th.c
@@ -1,3 +1,4 @@
+//Write a program to input a character and check whether it is a vowel or consonant using if–else.
 #include <stdio.h> 
 
 int main() {
@@ -13,4 +14,5 @@ int main() {
     }
 
     return 0; 
+
 }
