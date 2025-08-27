5995bf3fdd73fa91b9f95da2d3bf656bd63c6ddd
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 16:14:36 +0530

    Add files via upload

---
+++ 4rth.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    float radius;
+    float area;
+    float circumference;
+    printf("Enter radius of circle:");
+    scanf("%f",&radius);
+    circumference=2*3.14*radius;
+    printf("circumference of circle is:%f\n",circumference);
+    area=3.14*radius*radius;
+    printf("area of acircle is:%f\n", area);
+    return 0;
+};
