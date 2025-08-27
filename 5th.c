6ef3603af20b85ea20df5918357ce01516d1e86d
Cookiesandsharks <92847950+Cookiesandsharks@users.noreply.github.com>
2025-08-27 16:25:22 +0530

    Add files via upload

---
+++ 5th.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    float fahr;
+    float celsius;
+    int lower;
+    int upper;
+    int step;
+    lower=0;
+    upper=300;
+    step=20;
+    celsius=lower;
+    printf("Celsiusto Farenhite table\n");
+    while(celsius<=upper){
+        fahr=celsius*(9.0/5.0)+32;
+        printf("%3.0f %6.1f\n",celsius,fahr);
+        celsius=celsius+step;
+    }
+
+    
+}
